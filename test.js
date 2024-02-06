1c3e802e1ae83c6fe900094ec97ef71bcbf7f631
Julian Kuehnel <julian.kuehnel.ext@rise-world.com>
2024-02-06 16:08:50 +0100

    update

--- test.js
+++ test.js
@@ -1,17 +1,37 @@
+const leafletObject = {home: [47.72191016016923, 16.042789546032154]}
+
 const load = () => {
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    leafletObject.map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(leafletObject.map);
+
+    L.marker(leafletObject.home, {icon: houseIcon}).addTo(leafletObject.map);
 
     fetch("meine.gpx")
   .then((res) => res.text())
-  .then((text) => {
+  .then((text) => { 
     new L.GPX(text, {async: true}).on('loaded', function(e) {
-        map.fitBounds(e.target.getBounds());
-      }).addTo(map);
+      leafletObject.map.fitBounds(e.target.getBounds());
+        console.log(e)
+        console.log(text)
+        console.log(res)
+      }).addTo(leafletObject.map);
    })
   .catch((e) => console.error(e));
 
 }
+
+const fly = (location) => {
+  leafletObject.map.flyTo(leafletObject[location], 15)
+}
+
+
+
+const houseIcon = L.icon({
+  iconUrl: './style/img/house.png',
+
+  iconSize:     [38, 38], // size of the icon
+  iconAnchor:   [37, 37]
+});
